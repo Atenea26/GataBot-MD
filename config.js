2e0984a2941fe06adec41ffa1cc5cecda4949783
Atenea26 <154014175+Atenea26@users.noreply.github.com>
2023-12-16 17:28:17 -0300

    GataBot-line-MD

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ import { en, es, id, ar, pt, fr, hi } from "./lib/idiomas/total-idiomas.js"
 
 //⊱ ━━━━━.⋅ Añada los numeros a ser Propietario/a para GataBot-MD | Add the numbers to be Owner for GataBot-MD ⋅.━━━━ ⊰  
 
-global.owner = ["5493816932814"]
+global.owner = [
 ["5493816932814"], '𝙊𝙬𝙣𝙚𝙧 💖🐈', true], //𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿 💖🐈
 ["549381472028"], ["543816484147"]
 ["5492266466080"], 
@@ -25,7 +25,7 @@ global.owner = ["5493816932814"]
 
 //BETA: Si quiere evitar escribir el número que será bot en la consola, agregué desde aquí entonces:
 //Sólo aplica para opción 2 (ser bot con código de texto de 8 digitos)
-global.botNumberCode = "5493816932814" //Ejemplo: +59309090909
+global.botNumberCode = "" //Ejemplo: +59309090909
 global.confirmCode = ""
 
 global.mods = []
