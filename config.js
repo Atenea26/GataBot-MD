c277cfee149691a629a645c127083955e4028da2
Atenea <154014175+Atenea26@users.noreply.github.com>
2024-02-23 10:50:59 -0300

    Update config.js

--- config.js
+++ config.js
@@ -13,8 +13,8 @@ import { en, es, id, ar, pt } from "./lib/idiomas/total-idiomas.js"
 // [ES] > Agrega el número que será Bot y los que serán propietarios.
 // [EN] > Add the number that will be Bot and those that will be owners.
 global.owner = [
-["5492266466080", '𝙊𝙬𝙣𝙚𝙧 💖🐈', true], //𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿 💖🐈
-["593968585383"], 
+["5493814472028", '𝙊𝙬𝙣𝙚𝙧 💖🐈', true], //𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿 💖🐈
+["54 9 381 693-2814"], 
 ["593968263524"], ["5492266613038"], ["5217821153974"], ["5217294888993"]]
 
 global.mods = []
